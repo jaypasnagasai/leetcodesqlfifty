f6d4cd62f1c76501d0b079d4abf4510646d88e9e
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-02 09:26:30 -0700

    Create topthreesalaries.sql

---
+++ topthreesalaries.sql
@@ -0,0 +1,12 @@
+-- 185. Department Top Three Salaries
+
+SELECT Department,Employee,Salary
+FROM
+(SELECT
+d.name AS Department,
+e.name AS Employee,
+e.Salary AS Salary,
+dense_rank() over(PARTITION BY e.departmentId ORDER BY e.Salary DESC) AS rk
+FROM Employee e
+LEFT JOIN Department d ON e.departmentId = d.id) a
+WHERE rk <= 3
