c254967376bb9c10436e8e9babdd56c6a4a7edc3
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-02 08:42:00 -0700

    Create employeereport.sql

---
+++ employeereport.sql
@@ -0,0 +1,7 @@
+-- 1731. The Number of Employees Which Report to Each Employee
+
+SELECT M.employee_id, M.name, COUNT(*) as reports_count, ROUND(AVG(E.age),0) as average_age
+FROM Employees as E JOIN Employees as M
+ON E.reports_to = M.employee_id
+GROUP BY E.reports_to
+ORDER BY M.employee_id ASC
