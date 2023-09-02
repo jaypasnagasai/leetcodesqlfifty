b7fc3cd4c7415ebdd971eed40b9b2277b0f956aa
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-02 08:22:44 -0700

    Create prodsalesanalysisthree.sql

---
+++ prodsalesanalysisthree.sql
@@ -0,0 +1,12 @@
+-- 1070. Product Sales Analysis III
+
+select
+    product_id,
+    year as first_year,
+    quantity,
+    price
+from Sales
+where (product_id, year) in 
+    (select product_id, min(year) 
+     from Sales 
+     group by 1)
