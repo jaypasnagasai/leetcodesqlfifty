7dd5a966976d9a549be09b60085c9b087ac16b8b
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-02 08:54:26 -0700

    Create lastpersoninbus.sql

---
+++ lastpersoninbus.sql
@@ -0,0 +1,12 @@
+-- 1204. Last Person to Fit in the Bus
+
+WITH CTE AS (
+    SELECT 
+        turn, person_name, weight,
+        SUM(weight) OVER(ORDER BY turn ASC) AS tot_weight 
+    FROM Queue
+    ORDER BY turn
+)
+SELECT person_name
+FROM Queue q
+WHERE q.turn = (SELECT MAX(turn) FROM CTE WHERE tot_weight <= 1000);
